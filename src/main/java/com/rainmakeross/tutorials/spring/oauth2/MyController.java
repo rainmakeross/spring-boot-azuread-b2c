53f873356d3c9281564b9c6c18217ab02c0031fd
Daniel Yinanc <daniel.yinanc@gmail.com>
2017-10-05 16:47:35 -0400

    Init Push

---
+++ src/main/java/com/rainmakeross/tutorials/spring/oauth2/MyController.java
@@ -0,0 +1,33 @@
+package com.rainmakeross.tutorials.spring.oauth2;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+// our services are stateless and can be called from anywhere.
+@CrossOrigin
+@RestController
+public class MyController {
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @RequestMapping("/principal")
+    public Principal principal(Principal principal) {
+        return principal;
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/users")
+    public String users() {
+        return "I have users permission";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "I have admin permission";
+    }
+}
